928b65a340051b20a50f1263f2ed583d1e2d96b1
mansklackensjo <mklacken@gmail.com>
2024-10-23 23:59:25 +0200

    add 1.7

---
+++ Labb1/1.7.py
@@ -0,0 +1,13 @@
+# 3200, 5000, 1790, 8900 and 2300 
+
+monthly_cost = 100
+amount_of_months = int(input("Enter the number of months the server will run: "))
+
+purchase_cost = [3200, 5000, 1790, 8900, 2300]
+i = 0
+
+for _ in purchase_cost:
+    print("The server costing " + str(purchase_cost[i]) + " costs on average " + str(purchase_cost[i] / amount_of_months + monthly_cost) + "each month.")
+    i += 1
+
+print("Total cost: " + str(sum(purchase_cost) + (monthly_cost * 5 * amount_of_months)))
