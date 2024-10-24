44a28e392cec92cf37b3360ccba4697937725c28
mansklackensjo <104981316+mansklackensjo@users.noreply.github.com>
2024-10-24 14:33:05 +0200

    godkänd labb1

--- Labb1/1.4.py
+++ Labb1/1.4.py
@@ -4,7 +4,7 @@ print("This program displays a multiplication table between 0 to 9")
 table_number = int(input("which table do you want to see? "))
 
 if table_number < 9 and table_number > 0:
-    for i in range(10):
+    for i in range(11):
         answer = i * table_number
         print(str(i) + " * " + str(table_number) + " = " + str(answer))
 
