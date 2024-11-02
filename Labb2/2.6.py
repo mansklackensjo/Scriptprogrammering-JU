8fb1a81cf640d9c6ba4bb7377c935fb99d65ac9b
mansklackensjo <104981316+mansklackensjo@users.noreply.github.com>
2024-11-02 18:01:01 +0100

    2.6 modified

--- Labb2/2.6.py
+++ Labb2/2.6.py
@@ -1,18 +1,8 @@
-# def xxx(lastNumber:int):
-#     summa = 1
-#     for i in range(2, lastNumber + 1):
-#         summa += i*i
-
-#     return summa
-
-# print(xxx(3))
-
-
 def xxx(lastNumber:int):
-    summa = 0
+    summa = 0     
     for i in range(1, lastNumber + 1):
         summa += i*i
 
     return summa
 
-print(xxx(3))
+print(xxx(2))
